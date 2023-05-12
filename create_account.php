38a2be705291ecd807bebe8ac83e65338baefe4c
roua-timoumi <123248569+roua-timoumi@users.noreply.github.com>
2023-05-12 17:10:43 +0100

    create_account.php

--- create_account.php
+++ create_account.php
@@ -182,44 +182,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </div>
 </body>
 </html>
-<!--
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Create Account</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-    <h1>Create Account</h1>
-    <form action="#" method="POST">
-        <div class="form-group">
-            <label for="nom">Nom:</label>
-            <input type="text" class="form-control" id="nom" name="nom" required>
-        </div>
-        <div class="form-group">
-            <label for="prenom">Prénom:</label>
-            <input type="text" class="form-control" id="prenom" name="prenom" required>
-        </div>
-        <div class="form-group">
-            <label for="date_de_naissance">Date de naissance:</label>
-            <input type="date" class="form-control" id="date_de_naissance" name="date_de_naissance" required>
-        </div>
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" name="email" required>
-        </div>
-        <div class="form-group">
-            <label for="mot_de_passe">Mot de passe:</label>
-            <input type="password" class="form-control" id="mot_de_passe" name="mot_de_passe" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Create Account</button>
-    </form>
-</div>
-</body>
-</html>
--->
+
+
